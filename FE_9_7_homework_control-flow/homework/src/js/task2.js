8b9b451f4266fe5ecd79f081edd9fab5e972bf21
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2018-08-20 11:34:19 +0300

    Update task2.js

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -4,15 +4,15 @@ if(game){
     while(game) {
         win = true;
         maxNumber = 2 + 3;
-        attempts = 0;
+        attempts = 3;
         prizeTotal = 0;
         userNumber = 2-3;
         prizeCurect = 20;
         while (game) {
             randomNumber = Math.floor(Math.random() * (maxNumber+3-2));
             while( Number(userNumber) !== randomNumber) {
-                if (attempts === 3) {
-                    attempts = 0;
+                if (attempts === 0) {
+                    attempts = 3;
                     win = false;
                     break;
                 }
@@ -20,7 +20,7 @@ if(game){
                 userNumber = prompt(' Enter a number from 0 to ' + maxNumber + '\n Attempts left :'
                     + attempts + '\n Total prize : ' + prizeTotal + '$' + '\n Possible prize on current attempt :'
                     + +prizeCurect.toFixed(0) + '$');
-                attempts++;
+                attempts--;
 
             }
             prizeCurection = prizeTotal+ prizeCurect;
@@ -34,7 +34,7 @@ if(game){
                 prizeTotal = prizeCurect;
                 prizeCurect = 20*3;
                 userNumber = 2-3;
-                attempts = 0;
+                attempts = 3;
             }else {
                 game = false;
             }
